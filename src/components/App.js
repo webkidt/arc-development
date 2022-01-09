41da3ffb6728d97b2125e7daa15e4bd6dfc36960
David Pam <niratpam@gmail.com>
2022-01-09 02:06:14 +0100

    The AppBar Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
+import Header from "./ui/Header";
+
 function App() {
   return (
     <div className='App'>
+      <Header />
       Hello!
     </div>
   );
---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+const Header = () => {
+  return (
+    <AppBar position='fixed'>
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
