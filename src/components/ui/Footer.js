07552dbf92fd03b013533b73070c5a0f7476d507
David Pam <niratpam@gmail.com>
2022-02-11 20:40:21 +0100

    Footer Social Icons Link

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,11 +4,23 @@ import Grid from '@mui/material/Grid';
 import { styled } from '@mui/system';
 
 import footerAdornment from '../../assets/Footer Adornment.svg';
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
 
 // cSpell: disable
 const GridItemWrapper = styled(({ ...props }) => <Grid item {...props} />)({
   margin: '3em',
 });
+
+const SocialIcon = styled('img')(({ theme }) => ({
+  width: '4em',
+  height: '4em',
+  [theme.breakpoints.down('sm')]: {
+    width: '2.5em',
+    height: '2.5em',
+  },
+}));
 
 const FooterLink = styled(({ ...props }) => <Grid item {...props} />)({
   color: '#FFFFFF',
@@ -125,6 +137,44 @@ const Footer = ({ setValue, setSelectedIndex, ...props }) => {
         src={footerAdornment}
         sx={{ width: { xs: '15em', sm: '21em', md: '25em' }, verticalAlign: 'bottom' }}
       />
+      <Grid
+        container
+        justifyContent='flex-end'
+        spacing={2}
+        sx={{
+          position: 'absolute',
+          marginTop: '-6em',
+          right: { xs: '0.6em', sm: '1.5em' },
+        }}
+      >
+        <Grid
+          item
+          component='a'
+          href='https://www.facebook.com'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <SocialIcon alt='facebook logo' src={facebook} />
+        </Grid>
+        <Grid
+          item
+          component='a'
+          href='https://www.twitter.com'
+          rel='nopener noreferrer'
+          target='_blank'
+        >
+          <SocialIcon alt='twitter logo' src={twitter} />
+        </Grid>
+        <Grid
+          item
+          component='a'
+          href='https://www.instagram.com'
+          rel='nopener noreferrer'
+          target='_blank'
+        >
+          <SocialIcon alt='instagram logo' src={instagram} />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
