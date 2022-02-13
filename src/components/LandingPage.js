030740bedd81802fa0f9b5b8db4a7564fe70b957
David Pam <niratpam@gmail.com>
2022-02-13 04:11:12 +0100

    Landing Page Hero Block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,7 @@
 import Lottie from 'react-lottie';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import ButtonArrow from './ui/ButtonArrow';
 
 // cSpell: disable
 import animationData from '../animations/landinganimation/data';
@@ -13,7 +16,33 @@ const LandingPage = () => {
     },
   };
 
-  return <Lottie options={defaultOptions} height={'100%'} width={'100%'} />;
+  return (
+    <Grid container direction='column'>
+      <Grid item>
+        <Grid container>
+          <Grid item>
+            <div>
+              Bringing West Coast Technology <br /> to the Midwest
+            </div>
+            <Grid container>
+              <Grid item>
+                <Button variant='contained'>Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined'>
+                  Learn More
+                  <ButtonArrow width={15} height={15} fill='red' />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LandingPage;
