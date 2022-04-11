8b9100f02d472edec4d6288547c9bd2e3dc483f5
David Pam <niratpam@gmail.com>
2022-04-11 03:32:37 +0100

    Feat: Custom Software Development Row 2 & 3
    * Root-Cause Row: Image of tree with extended roots and paragraph texts.
    * Animation 2 Row: Automation and UX texts and animations.

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -12,8 +12,12 @@ import forwardArrow from '../assets/forwardArrow.svg';
 import lightbulb from '../assets/bulb.svg';
 import cash from '../assets/cash.svg';
 import stopwatch from '../assets/stopwatch.svg';
+import roots from '../assets/root.svg';
+
 import documentsAnimation from '../animations/documentsAnimation/data';
 import scaleAnimation from '../animations/scaleAnimation/data.json';
+import automationAnimation from '../animations/automationAnimation/data.json';
+import uxAnimation from '../animations/uxAnimation/data';
 
 const ArrowContainer = styled(Grid)({ marginTop: '0.5em' });
 
@@ -39,6 +43,24 @@ const CustomSoftware = ({ setSelectedIndex }) => {
     loop: true,
     autoplay: false,
     animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  const automationOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: automationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  const uxOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: uxAnimation,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
     },
@@ -202,6 +224,89 @@ const CustomSoftware = ({ setSelectedIndex }) => {
                 Whether you’re a large brand, just getting started, or taking off right
                 now, our application architecture ensures pain-free growth and
                 reliability.
+              </Typography>
+            </Grid>
+          </Grid>
+        </ItemContainer>
+      </Grid>
+      <Grid container item direction='row' my='20em'>
+        <Grid container item direction='column' alignItems='center'>
+          <Grid item>
+            <img
+              src={roots}
+              alt='tree with roots extending out'
+              height='450em'
+              width='450em'
+            />
+          </Grid>
+          <ItemContainer>
+            <Typography variant='h4' align='center'>
+              Root-Cause Analysis
+            </Typography>
+            <Typography paragraph variant='body1' align='center'>
+              Many problems are merely symptoms of larger, underlying issues.
+            </Typography>
+            <Typography paragraph variant='body1' align='center'>
+              We can help you thoroughly examine all areas of your business to develop a
+              holistic plan for the most effective implementation of technology.
+            </Typography>
+          </ItemContainer>
+        </Grid>
+      </Grid>
+      <Grid container item direction='row' justifyContent='space-between'>
+        <ItemContainer container item md>
+          <Grid container item md direction='column'>
+            <Grid item>
+              <Typography variant='h4'>Automation</Typography>
+            </Grid>
+            <Grid item>
+              <Typography paragraph variant='body1'>
+                Why waste time when you don’t have to?
+              </Typography>
+              <Typography paragraph variant='body1'>
+                We can help you identify processes with time or event based actions which
+                can now easily be automated.
+              </Typography>
+              <Typography paragraph variant='body1'>
+                Increasing efficiency increases profits, leaving you more time to focus on
+                your business, not busywork.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={automationOptions}
+              isStopped={true}
+              style={{ maxHeight: 290, maxWidth: 280 }}
+            />
+          </Grid>
+        </ItemContainer>
+        <ItemContainer container item md>
+          <Grid item md>
+            <Lottie
+              options={uxOptions}
+              isStopped={true}
+              style={{ maxHeight: 310, maxWidth: 155 }}
+            />
+          </Grid>
+          <Grid container item md direction='column'>
+            <Grid item>
+              <Typography variant='h4' align='right'>
+                User Experience Design
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography paragraph variant='body1' align='right'>
+                A good design that isn’t usable isn’t a good design.
+              </Typography>
+              <Typography paragraph variant='body1' align='right'>
+                So why are so many pieces of software complicated, confusing, and
+                frustrating?
+              </Typography>
+              <Typography paragraph variant='body1' align='right'>
+                By prioritizing users and the real ways they interact with technology
+                we’re able to develop unique, personable experiences that solve problems
+                rather than create new ones.
               </Typography>
             </Grid>
           </Grid>
